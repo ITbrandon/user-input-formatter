9c57106c85b8bcc238226042b2d9758c963a1211
Itbrandon <brandoniticka@gmail.com>
2024-02-14 11:50:25 -0800

    final-update

--- client.js
+++ client.js
@@ -43,28 +43,20 @@ class formatter {
 
     //Removing inevitable Commas from String
     let userName = finalString.replace(/,/g , "");
-
-    //Adding a Way to incorporate a Last Name
-    let fullName = userName.replace("%", " ");
     
-    //Grabbing index of the First Letter of Last Name
-    let lastNameStartIndex = (userName.indexOf("%")) + 1
+    //Checking If user put Last Name
+    if(userName.includes('%'))
+    {
+      //Taking Input and Passing to the Function
+      this.formatLastName(userName);
+    }
 
-    //Slicing full Name into two different Strings
-    let lastName = fullName.slice(lastNameStartIndex);
-    let firstName = fullName.slice(0, lastNameStartIndex);
+    else
+    {
+      //Printing Name to Screen
+      this.username.textContent = `Your Name is ${userName}`;
+    }
 
-    //Using Index to Grab the Letter then Capitalizing It
-    let capital = lastName[0].toUpperCase();
-
-    //Replace initial Letter with the Capitalized one
-    let finalLastName = lastName.replace(lastName.charAt(0), capital);
-    
-    //Combining the Two Strings
-    let finalName = firstName + finalLastName;
-
-    //Printing Name to Screen
-    this.username.textContent = `Your Name is ${finalName}`;
     }
 
     else {
@@ -72,6 +64,30 @@ class formatter {
       this.username.textContent = "Please Enter Your Name!";
       console.error('No Name Entered')
     }
+  }
+
+  formatLastName = (name) => {
+     //Adding a Way to incorporate a Last Name
+     let fullName = name.replace("%", " ");
+    
+     //Grabbing index of the First Letter of Last Name
+     let lastNameStartIndex = (name.indexOf("%")) + 1
+ 
+     //Slicing full Name into two different Strings
+     let lastName = fullName.slice(lastNameStartIndex);
+     let firstName = fullName.slice(0, lastNameStartIndex);
+ 
+     //Using Index to Grab the Letter then Capitalizing It
+     let capital = lastName[0].toUpperCase();
+ 
+     //Replace initial Letter with the Capitalized one
+     let finalLastName = lastName.replace(lastName.charAt(0), capital);
+     
+     //Combining the Two Strings
+     let finalName = firstName + finalLastName;
+ 
+     //Printing Name to Screen
+     this.username.textContent = `Your Name is ${finalName}`;
   }
 }
 
